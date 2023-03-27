dbe9311f322ac2d755372300dfe1a87476d643f8
Thao Tran H D <tranhdieuthao95@gmail.com>
2023-03-27 20:57:34 +0700

    Style rounded corner

--- App.js
+++ App.js
@@ -27,7 +27,11 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} /> 
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}  
+        {courseGoals.map((goal) => (
+        <View key={goal} style={styles.goalItem}>
+        <Text style={styles.goalText}>{goal}</Text>   
+        </View>
+        ))}
       </View>
     </View>
   );
@@ -56,6 +60,15 @@ textInput: {
   padding: 8
 },
 goalsContainer: {
-  flex: 4 
+  flex: 5 
+},
+goalItem: {
+  margin: 8,
+  padding: 8,
+  borderRadius: 6,
+  backgroundColor:'#5e0acc',
+},
+goalText: {
+  color: 'white' 
 }
 });
